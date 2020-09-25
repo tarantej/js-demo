db11586951797a64dd9ff737731bf4f4c2530f3f
Taran <21302579+tarantej@users.noreply.github.com>
2020-09-26 09:35:54 +1200

    Project Last List - UI

---
+++ projects/task-list/js/app.js
@@ -0,0 +1,75 @@
+// Test
+
+// alert('working')
+
+
+//  Define UI Variables
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
+
+//  Load all event listeners
+
+loadEventListeners();
+
+function loadEventListeners() {
+    //  Add task Event
+    form.addEventListener('submit', addTask);
+
+    //  Add Task
+
+    function addTask(e) {
+
+        //  Check for value
+
+        if (taskInput.value === '') {
+            alert('Task Input cannot be blank');
+        }
+
+        console.log(taskInput);
+
+        //         //  Create List Item
+
+        //         const listItem = document.createElement('li');
+
+        //         //  Add Collection Item class to newly created list item
+
+        //         listItem.className = 'collection-item';
+
+        //         //  Create Text Node and append to listItem
+
+        //         listItem.appendChild(document.createTextNode(taskInput.value));
+
+        //         //  Create new link element
+
+        //         const link = document.createElement('a');
+
+        //         //  Add Link class
+
+        //         link.className = 'delete-item secondary-content red';
+
+        //         //  Add link html
+
+        //         link.innerHTML = "<i class='material-icons'>remove_circle</i>"
+
+        //         //  Append Link to listItem
+
+        //         listItem.appendChild(link);
+
+        //         //  Append listItem to List
+
+        //         taskList.appendChild(listItem);
+
+        //         //  Clear Input
+
+        //         taskInput.value = '';
+
+
+        e.preventDefault();
+    }
+}
---
+++ projects/task-list/js/test.js
@@ -0,0 +1,36 @@
+// alert('working');
+
+let abc = 123;
+
+console.log(abc);
+
+// const form = document.querySelector('#task-form');
+// const taskList = document.querySelector('.collection');
+// const clearBtn = document.querySelector('.clear-tasks');
+// const filter = document.querySelector('#filter');
+// const taskInput = document.querySelector('#task');
+
+
+// //  Load all event listeners
+
+// loadEventListeners();
+
+// function loadEventListeners() {
+//     //  Add task Event
+//     form.addEventListener('submit', addTask);
+
+//     //  Add Task
+
+//     function addTask(e) {
+
+//         //  Check for value
+
+//         if (taskInput.value === '') {
+//             alert('Task Input cannot be blank');
+//         }
+
+//         console.log(taskInput);
+//     }
+
+//     e.preventDefault();
+// }
